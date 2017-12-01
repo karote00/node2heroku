5e3fc4e59cf27bab0c2f4b66ed536fbdf36621de
Asa Tsai <karoteglathe@gmail.com>
2017-12-01 14:23:28 +0800

    split client and server

--- server/app.js
+++ server/app.js
@@ -1,18 +1,34 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
+const bodyParser	= require('body-parser');
 const path = require('path');
-
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 // Setup logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+// app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+// app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.set('/', path.join(__dirname, '/'));
+// app.set('view engine', 'ejs');
 
 // Always return the main index.html, so react-router render the route in the client
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+// });
+// app.get('/', (req, res) => res.render('index'));
+// app.get('/', (req, res) => {
+// 	// res.render('index')
+// 	res.sendFile(path.resolve(__dirname, '..', 'public/index.html'));
+// 	// res.send('Hello World');
+// });
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'client/build/index.html'));
 });
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -5,5 +5,8 @@ const app = require('./app');
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+	console.log(`Listening on ${ PORT }`);
 });
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
